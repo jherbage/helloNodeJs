b73f8bdbf2da6675cc214aa624455b26a57cf9fa
jherbage <jasonherbage@googlemail.com>
2017-10-11 15:40:06 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -9,6 +9,6 @@ var express=require('express');
 var app = express();
 app.get('/', sayHello);
 var port = process.argv.length > 2 &&  parseInt(Number(process.argv[2])) == process.argv[2] && parseInt(process.argv[2]) < 65535 && parseInt(process.argv[2]) > 1024 ? parseInt(process.argv[2]) : 3000;
-ver addr = process.argv.length > 3 : process.argv[3] ? '127.0.0.1'
+var addr = process.argv.length > 3 ? process.argv[3] : '127.0.0.1'
 var server = app.listen(port, addr);
 app.use(express.static('public'));

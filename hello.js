5d73802f57c249c74b8c84570423d1b23bd11e45
jherbage <jasonherbage@googlemail.com>
2017-10-11 14:11:43 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -8,5 +8,5 @@ function sayHello(request, response) {
 var express=require('express');
 var app = express();
 app.get('/', sayHello);
-var server = app.listen(3000);
+var server = app.listen(process.argv.length > 3 &&  parseInt(Number(process.argv[2])) == process.argv[2] && process.argv[2] < 65535 && process.argv[2] > 1024 ? process.argv[2] : 3000);
 app.use(express.static('public'));

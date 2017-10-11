1aca36934bd064b06041aa4f18986526868d32bd
jherbage <jasonherbage@googlemail.com>
2017-10-11 14:32:04 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 cd ~
 sudo apt-get update
-sudo apt-get install -y git npm node nodejs-legacy
+sudo apt-get install -y git 
+sudo apt-get install -y node 
+sudo apt-get install -y nodejs-legacy
+sudo apt-get install -y npm
+rm -rf *
 git clone https://github.com/jherbage/helloNodeJs.git
 cd helloNodeJs
 sudo npm install -g http-server

6706a8dac1fd978b339ee6bcccd8ce7348549345
jherbage <jasonherbage@googlemail.com>
2017-11-17 10:35:40 +0000

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const os = require('os')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World from host '+os.hostname()+'!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000 and host '+os.hostname()+'!'))
